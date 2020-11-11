0311feb738927bf5fd7c4f7b06df6cbf0fae6b0d
Ed Halferty <edhalferty@gmail.com>
2020-11-11 08:01:36 -0800

    Implement arbitration, rough draft

--- ncr5380.cpp
+++ ncr5380.cpp
@@ -1,113 +1,181 @@
-/* Arduino ADNS2620 Library
- * Can be used to interface between an ATmega328 (Arduino) and the ADNS2620 Mouse Sensor
- * Copyright 2010 SparkFun ElectronicS
- * Written by Ryan Owens
+/* Arduino NCR5380 Library
+ * Copyright 2020 Edward Halferty
 */
 
-#include "adns2620.h"
+// NCRduino
+// ADBtooth
 
-//Constructor sets the pins used for the mock 'i2c' communication
-ADNS2620::ADNS2620(int sda, int scl)
-{
-	_sda = sda;
-	_scl = scl;
+#include "ncr5380.h"
+
+//Constructor sets the pins to use for the NCR5380 connection
+NCR5380::NCR5380(int cs_, int drq, int irq, int ior_, int ready, int dack_, int eop_, int reset_, int iow_, int a0,
+                   int a1, int a2, int d0, int d1, int d2, int d3, int d4, int d5, int d6, int d7)
+{ SET_PIN_NUMBERS(); }
+
+void NCR5380::setLoggingEnabled(bool x) { loggingEnabled = x; }
+
+void NCR5380::setScsiId(int x) { scsiId = x; }
+
+void NCR5380::write(byte addr, byte data) {
+  SET_ADDR(addr);
+  SET_DATA(data);
+  PULSE_WRITE_PINS();
 }
 
-//Configures the communication pins for their initial state
-void ADNS2620::begin()
-{
-	pinMode(_sda, OUTPUT);
-	pinMode(_scl, OUTPUT);
+byte NCR5380::read(byte addr) {
+  SET_ADDR(addr);
+  SET_DATA_DIRECTION(INPUT);
+  delay(1);
+  SET_READ_PINS();
+  delay(1);
+  byte res = (digitalRead(_d7) << 7) | (digitalRead(_d6) << 6) | (digitalRead(_d5) << 5) | (digitalRead(_d4) << 4) |
+             (digitalRead(_d3) << 3) | (digitalRead(_d2) << 2) | (digitalRead(_d1) << 1) | digitalRead(_d0);
+  delay(1);
+  CLEAR_READ_PINS();
+  SET_DATA_DIRECTION(OUTPUT);
+  return res;
 }
 
-//Essentially resets communication to the ADNS2620 module
-void ADNS2620::sync()
-{
-    digitalWrite(_scl, HIGH);
-    delay(1);
-	digitalWrite(_scl, LOW);
-    delay(1);
-	digitalWrite(_scl, HIGH);
-    delay(100);
+bool NCR5380::poll(int reg1, byte bit1, byte val1) { return poll2(reg1, bit1, val1, reg1, bit1, val1); }
+
+bool NCR5380::poll2(int reg1, byte bit1, byte val1, int reg2, byte bit2, byte val2) {
+  for (int i = NUM_POLL_ITERATIONS; i > 0; i--) {
+    if ((NCR5380_read(reg1) & bit1) == val1) return true;
+    if ((NCR5380_read(reg2) & bit2) == val2) return true;
+  }
+  return false;
 }
 
-//Reads a register from the ADNS2620 sensor. Returns the result to the calling function.
-//Example: value = mouse.read(CONFIGURATION_REG);
-char ADNS2620::read(char address)
-{
-    char value=0;
-	pinMode(_sda, OUTPUT); //Make sure the SDIO pin is set as an output.
-    digitalWrite(_scl, HIGH); //Make sure the clock is high.
-    address &= 0x7F;    //Make sure the highest bit of the address byte is '0' to indicate a read.
+bool NCR5380::arbitrate() {
+  if (loggingEnabled) { Serial.print("Trying to arbitrate. ID=");Serial.print(scsiId);Serial.print("\n"); }
+  //Set the phase bits to 0, otherwise the NCR5380 won't drive the data bus during SELECTION.
+  NCR5380_write(TARGET_COMMAND_REG, 0);
+  NCR5380_write(OUTPUT_DATA_REG, ID_MASK);
+  NCR5380_write(MODE_REG, MR_ARBITRATE);
+  // Wait for BUS FREE phase
+  bool ok = poll2(MODE_REG, MR_ARBITRATE, 0, INITIATOR_COMMAND_REG, ICR_ARBITRATION_PROGRESS, ICR_ARBITRATION_PROGRESS);
+  if (!ok) {
+    if (loggingEnabled) { Serial.print("Arbitration timeout\n"); }
+    return false;
+  }
+  //Check for lost arbitration
+  if ((NCR5380_read(INITIATOR_COMMAND_REG) & ICR_ARBITRATION_LOST) ||
+      (NCR5380_read(CURRENT_SCSI_DATA_REG) & ID_HIGHER_MASK) ||
+      (NCR5380_read(INITIATOR_COMMAND_REG) & ICR_ARBITRATION_LOST))
+  {
+    if (loggingEnabled) { Serial.print("Lost arbitration. deasserting MR_ARBITRATE\n"); }
+    return false;
+  }
+  //After/during arbitration, BSY should be asserted.
+  NCR5380_write(INITIATOR_COMMAND_REG, ICR_ASSERT_SEL | ICR_ASSERT_BSY);
+  return true;
+}
+
+byte NCR5380::readCurrentScsiDataReg() { return read(CURRENT_SCSI_DATA_REG); }
+
+//Initializes the device, resets the SCSI bus, etc.
+void NCR5380::begin() {
+    SET_INITIAL_PIN_DIRECTIONS();
+    SET_INITIAL_PIN_VALUES();
+    PULSE_RESET_PIN();
+    RESET_BUS();
+    CLEAR_INTERRUPT_CONDITIONS();
+}
+
+void NCR5380::test() {
+  bool res = arbitrate();
+  Serial.print("res=");Serial.print(res);Serial.print("\n");
+}
+
+
+// //Essentially resets communication to the ADNS2620 module
+// void ADNS2620::sync()
+// {
+//     digitalWrite(_scl, HIGH);
+//     delay(1);
+// 	digitalWrite(_scl, LOW);
+//     delay(1);
+// 	digitalWrite(_scl, HIGH);
+//     delay(100);
+// }
+
+// //Reads a register from the ADNS2620 sensor. Returns the result to the calling function.
+// //Example: value = mouse.read(CONFIGURATION_REG);
+// char ADNS2620::read(char address)
+// {
+//     char value=0;
+// 	pinMode(_sda, OUTPUT); //Make sure the SDIO pin is set as an output.
+//     digitalWrite(_scl, HIGH); //Make sure the clock is high.
+//     address &= 0x7F;    //Make sure the highest bit of the address byte is '0' to indicate a read.
  
-    //Send the Address to the ADNS2620
-    for(int address_bit=7; address_bit >=0; address_bit--){
-        digitalWrite(_scl, LOW);  //Lower the clock
-        pinMode(_sda, OUTPUT); //Make sure the SDIO pin is set as an output.
+//     //Send the Address to the ADNS2620
+//     for(int address_bit=7; address_bit >=0; address_bit--){
+//         digitalWrite(_scl, LOW);  //Lower the clock
+//         pinMode(_sda, OUTPUT); //Make sure the SDIO pin is set as an output.
         
-        //If the current bit is a 1, set the SDIO pin. If not, clear the SDIO pin
-        if(address & (1<<address_bit)){
-            digitalWrite(_sda, HIGH);
-        }
-        else{
-            digitalWrite(_sda, LOW);
-        }
-        delayMicroseconds(10);
-        digitalWrite(_scl, HIGH);
-        delayMicroseconds(10);
-    }
+//         //If the current bit is a 1, set the SDIO pin. If not, clear the SDIO pin
+//         if(address & (1<<address_bit)){
+//             digitalWrite(_sda, HIGH);
+//         }
+//         else{
+//             digitalWrite(_sda, LOW);
+//         }
+//         delayMicroseconds(10);
+//         digitalWrite(_scl, HIGH);
+//         delayMicroseconds(10);
+//     }
     
-    delayMicroseconds(120);   //Allow extra time for ADNS2620 to transition the SDIO pin (per datasheet)
-    //Make SDIO an input on the microcontroller
-    pinMode(_sda, INPUT);	//Make sure the SDIO pin is set as an input.
-	digitalWrite(_sda, HIGH); //Enable the internal pull-up
+//     delayMicroseconds(120);   //Allow extra time for ADNS2620 to transition the SDIO pin (per datasheet)
+//     //Make SDIO an input on the microcontroller
+//     pinMode(_sda, INPUT);	//Make sure the SDIO pin is set as an input.
+// 	digitalWrite(_sda, HIGH); //Enable the internal pull-up
         
-    //Send the Value byte to the ADNS2620
-    for(int value_bit=7; value_bit >= 0; value_bit--){
-        digitalWrite(_scl, LOW);  //Lower the clock
-        delayMicroseconds(10); //Allow the ADNS2620 to configure the SDIO pin
-        digitalWrite(_scl, HIGH);  //Raise the clock
-        delayMicroseconds(10);
-        //If the SDIO pin is high, set the current bit in the 'value' variable. If low, leave the value bit default (0).    
-		//if((ADNS_PIN & (1<<ADNS_sda)) == (1<<ADNS_sda))value|=(1<<value_bit);
-		if(digitalRead(_sda))value |= (1<<value_bit);
+//     //Send the Value byte to the ADNS2620
+//     for(int value_bit=7; value_bit >= 0; value_bit--){
+//         digitalWrite(_scl, LOW);  //Lower the clock
+//         delayMicroseconds(10); //Allow the ADNS2620 to configure the SDIO pin
+//         digitalWrite(_scl, HIGH);  //Raise the clock
+//         delayMicroseconds(10);
+//         //If the SDIO pin is high, set the current bit in the 'value' variable. If low, leave the value bit default (0).    
+// 		//if((ADNS_PIN & (1<<ADNS_sda)) == (1<<ADNS_sda))value|=(1<<value_bit);
+// 		if(digitalRead(_sda))value |= (1<<value_bit);
 
-    }
+//     }
     
-    return value;
-}	
+//     return value;
+// }	
 
-//Writes a value to a register on the ADNS2620.
-//Example: mouse.write(CONFIGURATION_REG, 0x01);
-void ADNS2620::write(char address, char value)
-{
-	pinMode(_sda, OUTPUT);	//Make sure the SDIO pin is set as an output.
-    digitalWrite(_scl, HIGH);          //Make sure the clock is high.
-    address |= 0x80;    //Make sure the highest bit of the address byte is '1' to indicate a write.
+// //Writes a value to a register on the ADNS2620.
+// //Example: mouse.write(CONFIGURATION_REG, 0x01);
+// void ADNS2620::write(char address, char value)
+// {
+// 	pinMode(_sda, OUTPUT);	//Make sure the SDIO pin is set as an output.
+//     digitalWrite(_scl, HIGH);          //Make sure the clock is high.
+//     address |= 0x80;    //Make sure the highest bit of the address byte is '1' to indicate a write.
 
-    //Send the Address to the ADNS2620
-    for(int address_bit=7; address_bit >=0; address_bit--){
-        digitalWrite(_scl, LOW); //Lower the clock
+//     //Send the Address to the ADNS2620
+//     for(int address_bit=7; address_bit >=0; address_bit--){
+//         digitalWrite(_scl, LOW); //Lower the clock
         
-        delayMicroseconds(10); //Give a small delay (only needed for the first iteration to ensure that the ADNS2620 relinquishes
-                    //control of SDIO if we are performing this write after a 'read' command.
+//         delayMicroseconds(10); //Give a small delay (only needed for the first iteration to ensure that the ADNS2620 relinquishes
+//                     //control of SDIO if we are performing this write after a 'read' command.
         
-        //If the current bit is a 1, set the SDIO pin. If not, clear the SDIO pin
-        if(address & (1<<address_bit))digitalWrite(_sda, HIGH);
-        else digitalWrite(_sda, LOW);
-        delayMicroseconds(10);
-        digitalWrite(_scl, HIGH);
-        delayMicroseconds(10);
-    }
+//         //If the current bit is a 1, set the SDIO pin. If not, clear the SDIO pin
+//         if(address & (1<<address_bit))digitalWrite(_sda, HIGH);
+//         else digitalWrite(_sda, LOW);
+//         delayMicroseconds(10);
+//         digitalWrite(_scl, HIGH);
+//         delayMicroseconds(10);
+//     }
     
-    //Send the Value byte to the ADNS2620
-    for(int value_bit=7; value_bit >= 0; value_bit--){
-        digitalWrite(_scl, LOW);  //Lower the clock
-        //If the current bit is a 1, set the SDIO pin. If not, clear the SDIO pin
-        if(value & (1<<value_bit))digitalWrite(_sda, HIGH);
-        else digitalWrite(_sda, LOW);
-        delayMicroseconds(10);
-        digitalWrite(_scl, HIGH);
-        delayMicroseconds(10);
-    }
-}
+//     //Send the Value byte to the ADNS2620
+//     for(int value_bit=7; value_bit >= 0; value_bit--){
+//         digitalWrite(_scl, LOW);  //Lower the clock
+//         //If the current bit is a 1, set the SDIO pin. If not, clear the SDIO pin
+//         if(value & (1<<value_bit))digitalWrite(_sda, HIGH);
+//         else digitalWrite(_sda, LOW);
+//         delayMicroseconds(10);
+//         digitalWrite(_scl, HIGH);
+//         delayMicroseconds(10);
+//     }
+// }
--- ncr5380.h
+++ ncr5380.h
@@ -1,39 +1,90 @@
-/* Arduino ADNS2620 Library
- * Copyright 2010 SparkFun Electronic
- * Written by Ryan Owens
+/* Arduino NCR5380 Library
+ * Copyright 2020 Edward Halferty
 */
 
-#ifndef adns2620_h
-#define adns2620_h
+#ifndef ncr5380_h
+#define ncr5380_h
 
 #include <avr/pgmspace.h>
 #include "arduino.h"
+#include "linux_ncr5380.h"
 
-class ADNS2620
-{
+#define NUM_POLL_ITERATIONS 1000
+
+#define SET_PIN_NUMBERS() _cs_=cs_;_drq=drq;_irq=irq;_ior_=ior_;_ready=ready;_dack_=dack_;_eop_=eop_;_reset_=reset_;\
+_iow_=iow_;_a0=a0;_a1=a1;_a2=a2;_d0=d0;_d1=d1;_d2=d2;_d3=d3;_d4=d4;_d5=d5;_d6=d6;_d7=d7;
+
+#define SET_INITIAL_PIN_DIRECTIONS() pinMode(_cs_, OUTPUT);pinMode(_drq, INPUT);pinMode(_irq, INPUT);\
+pinMode(_ior_, OUTPUT);pinMode(_ready, INPUT);pinMode(_dack_, OUTPUT);pinMode(_eop_, OUTPUT);pinMode(_reset_, OUTPUT);\
+pinMode(_iow_, OUTPUT);pinMode(_a0, OUTPUT);pinMode(_a1, OUTPUT);pinMode(_a2, OUTPUT);pinMode(_d0, OUTPUT);\
+pinMode(_d1, OUTPUT);pinMode(_d2, OUTPUT);pinMode(_d3, OUTPUT);pinMode(_d4, OUTPUT);pinMode(_d5, OUTPUT);\
+pinMode(_d6, OUTPUT);pinMode(_d7, OUTPUT);
+
+#define SET_INITIAL_PIN_VALUES() digitalWrite(_cs_, HIGH);/* Initially disabled */ digitalWrite(_reset_, HIGH);\
+digitalWrite(_dack_, HIGH);digitalWrite(_eop_, HIGH);
+
+#define SET_DATA_DIRECTION(x) pinMode(_d0, x);pinMode(_d1, x);pinMode(_d2, INPUT);pinMode(_d3, INPUT);\
+pinMode(_d4, INPUT);pinMode(_d5, INPUT);pinMode(_d6, INPUT);pinMode(_d7, INPUT);
+
+#define SET_DATA(x) digitalWrite(_d0, x & 0x01);digitalWrite(_d1, (x >> 1) & 0x01);digitalWrite(_d2, (x >> 2) & 0x01);\
+digitalWrite(_d3, (x >> 3) & 0x01);digitalWrite(_d4, (x >> 4) & 0x01);digitalWrite(_d5, (x >> 5) & 0x01);\
+digitalWrite(_d6, (x >> 6) & 0x01);digitalWrite(_d7, (x >> 7) & 0x01);
+#define SET_ADDR(x) digitalWrite(_a0, x & 0x01);digitalWrite(_a1, (x >> 1) & 0x01);digitalWrite(_a2, (x >> 2) & 0x01);
+#define SET_WRITE_PINS()   digitalWrite(_iow_, LOW); digitalWrite(_cs_,  LOW);
+#define CLEAR_WRITE_PINS() digitalWrite(_cs_,  HIGH);digitalWrite(_iow_, HIGH);
+#define SET_READ_PINS()    digitalWrite(_ior_, LOW); digitalWrite(_cs_,  LOW);
+#define CLEAR_READ_PINS()  digitalWrite(_cs_,  HIGH);digitalWrite(_ior_, HIGH);
+#define PULSE_WRITE_PINS() delay(1);SET_WRITE_PINS();delay(1);CLEAR_WRITE_PINS();delay(1);
+#define PULSE_RESET_PIN() digitalWrite(_reset_, LOW); delay(100); digitalWrite(_reset_, HIGH); delay(100);
+
+#define NCR5380_read read
+#define NCR5380_write write
+#define NCR5380_poll_politely poll
+#define NCR5380_poll_politely2 poll2
+
+#define RESET_BUS() write(INITIATOR_COMMAND_REG, ICR_ASSERT_RST);delay(1);write(INITIATOR_COMMAND_REG, 0);
+#define CLEAR_INTERRUPT_CONDITIONS() (void)NCR5380_read(RESET_PARITY_INTERRUPT_REG);
+
+#define ID_MASK 1 << scsiId
+#define ID_HIGHER_MASK 0b11111111 << scsiId + 1
+
+
+class NCR5380 {
 	public:
-		ADNS2620(int sda, int scl);
+		NCR5380(int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int);
 		void begin();
-		void sync();
-		char read(char address);
-		void write(char address, char value);
+		byte readCurrentScsiDataReg();
+		void setLoggingEnabled(bool);
+		void setScsiId(int);
+		void test();
 	private:
-		int _sda;
-		int _scl;
+    	int _cs_;
+	    int _drq;
+	    int _irq;
+	    int _ior_;
+	    int _ready;
+	    int _dack_;
+	    int _eop_;
+	    int _reset_;
+	    int _iow_;
+	    int _a0;
+	    int _a1;
+	    int _a2;
+	    int _d0;
+	    int _d1;
+	    int _d2;
+	    int _d3;
+	    int _d4;
+	    int _d5;
+	    int _d6;
+	    int _d7;
+	    bool loggingEnabled = false;
+	    int scsiId = 7;
+	    void write(byte, byte);
+	    byte read(byte);
+	    bool arbitrate();
+	    bool poll(int, byte, byte);
+	    bool poll2(int, byte, byte, int, byte, byte);
 };
-
-/* Register Map for the ADNS2620 Optical Mouse Sensor */
-#define CONFIGURATION_REG   0x40
-#define STATUS_REG          0x41
-#define DELTA_Y_REG         0x42
-#define DELTA_X_REG         0x43
-#define SQUAL_REG           0x44
-#define MAXIMUM_PIXEL_REG   0x45
-#define MINIMUM_PIXEL_REG   0x46
-#define PIXEL_SUM_REG       0x47
-#define PIXEL_DATA_REG      0x48
-#define SHUTTER_UPPER_REG   0x49
-#define SHUTTER_LOWER_REG   0x4A
-#define FRAME_PERIOD		0x4B
 
 #endif
